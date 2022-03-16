fcf0018b212cf0572172bf7ae963a0aca5f0f3ae
Gunyoung-Kim <gun025bba@naver.com>
2022-03-16 21:25:39 +0900

    api: ContentRestController Security 적용

--- infokt-api/src/main/kotlin/com/gunyoung/infokt/api/controller/ContentController.kt
+++ infokt-api/src/main/kotlin/com/gunyoung/infokt/api/controller/ContentController.kt
@@ -6,26 +6,37 @@ import com.gunyoung.infokt.common.model.ContentEntity
 import com.gunyoung.infokt.common.model.NotMyResourceException
 import com.gunyoung.infokt.common.service.ContentService
 import com.gunyoung.infokt.common.service.LinkService
+import com.gunyoung.infokt.common.service.SpaceService
 import com.gunyoung.infokt.common.service.UserService
 import com.gunyoung.infokt.common.util.getSessionUserEmail
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.*
 import javax.validation.Valid
 
 @RestController
 class ContentRestController(
     val contentService: ContentService,
+    val spaceService: SpaceService,
     val userService: UserService,
     val linkService: LinkService
 ) {
 
+    /**
+     * userId == contentDto.hostId 는 AOP 로 처리하자
+     */
+    @PreAuthorize("@userSecurityService.checkIsMineById(#userId)")
     @PostMapping("/contents/{userId}")
     fun createContent(
         @PathVariable userId: Long,
         @Valid @ModelAttribute contentDto: ContentDto
-    ) {
-
+    ) : Unit {
+        // todo
     }
 
+
+    /**
+     * isSessionUsers PreAuthorize 로 옮길까 고민
+     */
     @DeleteMapping("/contents/{id}")
     fun deleteContent(
         @PathVariable("id") id: Long
--- infokt-common/src/main/kotlin/com/gunyoung/infokt/common/service/SecurityServices.kt
+++ infokt-common/src/main/kotlin/com/gunyoung/infokt/common/service/SecurityServices.kt
@@ -1,5 +1,6 @@
 package com.gunyoung.infokt.common.service
 
+import com.gunyoung.infokt.common.model.UserEntity
 import com.gunyoung.infokt.common.repository.UserRepository
 import com.gunyoung.infokt.common.util.getSessionUserEmail
 
@@ -7,10 +8,18 @@ interface SecurityService {
 }
 
 interface UserSecurityService {
+    fun checkIsMineById(id: Long): Boolean
     fun checkIsMineByEmail(email: String): Boolean
 }
 
 class UserSecurityServiceImpl(val userRepository: UserRepository) : UserSecurityService {
+    override fun checkIsMineById(id: Long): Boolean {
+        val userEntity = findUserByEmail(getSessionUserEmail()) ?: return false
+        return userEntity.id == id
+    }
+
+    private fun findUserByEmail(email: String): UserEntity? = userRepository.findByEmail(email)
+
     override fun checkIsMineByEmail(email: String): Boolean {
         return getSessionUserEmail().let {
             checkExistEmail(it) && it == email
